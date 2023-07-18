415b966e8f9067c7c15ef46d9e68b8fb4d8ed3bb
Arnav Gaddam <108426505+arnavgaddam@users.noreply.github.com>
2023-07-17 21:28:47 -0700

    fetch method to implement search functionality

--- client/app.py
+++ client/app.py
@@ -1,6 +1,7 @@
 from predictor import Predictor
 from scraper import NBAScraper
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, jsonify, make_response
+import requests
 
 
 # predictor = Predictor()
@@ -11,14 +12,17 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/",)
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/search")
+@app.route("/search", methods = ['POST'])
 def search():
-    return render_template('')
+    req = request.get_json()
+    print(req)
+    res = make_response(jsonify({"message": "JSON receive"}), 200)
+    return res
 
 @app.route("/prediction", methods=['POST', 'GET'])
 def predict_stats():
