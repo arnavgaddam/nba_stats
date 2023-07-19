58f6fdfd3e8f1b09d6670713a26416d6f82e6ff9
Arnav Gaddam <108426505+arnavgaddam@users.noreply.github.com>
2023-07-18 21:03:00 -0700

    load model asynch

--- client/app.py
+++ client/app.py
@@ -4,9 +4,7 @@ from scraper import NBAScraper
 from threading import Thread
 from flask import Flask, url_for, render_template, request, jsonify, make_response, redirect
 import requests
-from celery import Celery
-from rq import Queue
-import redis
+from turbo_flask import Turbo
 from tasks import background_task
 
 # predictor = Predictor()
@@ -15,9 +13,9 @@ from tasks import background_task
 # predictor.train_model('pts', scraper.get_advanced_player_stats(playername))
 
 app = Flask(__name__)
+turbo = Turbo(app)
 scraper = NBAScraper()
-r = redis.Redis()
-q = Queue(connection=r)
+
     
 
 @app.route("/", methods=['GET', 'POST'])
@@ -37,15 +35,37 @@ def search():
     # res = make_response(jsonify({"playerID": scraper.player_to_id(req['playerName'].title())}), 200)
     return "<p> blank </p>"
 
+
+tasks = []
+results = []
+
 @app.route("/prediction/<playerID>", methods=['POST', 'GET'])
 def prediction(playerID):
     # pred = Predictor()
     # result = pred.train_model('pts', playerdf=scraper.get_advanced_player_stats(playerID=playerID)) 
-    job = q.enqueue(background_task, playerID)
-    print(f"Task {job.id} added to queue at {job.enqueued_at}. {len(q)} tasks in queue")
-    return render_template('prediction.html', player=600)
+    new_task_id = len(tasks)
+    task = Thread(target=do_work, kwargs={'playerID': playerID, 'task_id': new_task_id, 'results': results})
+    task.start()
+    tasks.append(task)
+    results.append(None)
+    return render_template('prediction.html', player=new_task_id)
+
+def do_work(task_id, results, playerID):
+    pred = Predictor()
+    results[task_id] = f"finished task {pred.train_model('pts', playerdf=scraper.get_advanced_player_stats(playerID=playerID))}"
+
+@app.route("/task/<int:task_id>", methods=["GET"])
+def task_status(task_id):
+    assert 0 <= task_id < len(tasks)
+    if tasks[task_id].is_alive():
+        return jsonify({'status': 'running'})
+    try:
+        tasks[task_id].join()
+        return jsonify({'status': 'finished', 'result': results[task_id]})
+    except RuntimeError:
+        return jsonify({'status': 'not started'})
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
